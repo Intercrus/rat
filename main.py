66494399214ab3aaaa3d58ebeb75b9f8aa2ce998
Intercrus <squadew@yandex.ru>
2021-11-16 00:04:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+class Rat:
+    def __init__(self):
+        pass
+
+    def system_info(self):
+        pass
+
+    def webcam(self):
+        pass
+
+    def microphone(self):
+        pass
+
+    def screen(self):
+        pass
+
+    def keyboard(self):
+        pass
+
+    def audio(self):
+        pass
